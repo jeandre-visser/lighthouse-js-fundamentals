2a3f81329c07249f36cb17dac5a13147e64c8516
Jeandre Visser <jeandrev1414@gmail.com>
2022-02-08 20:57:42 +0000

    Added more conditional examples

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -1,5 +1,35 @@
-const temperature = 12;
+// if statement examples
 
+
+// Example 1
+const raining = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+// Example 2
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+// Example 3
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+// Example 4
+const temperature = 12;
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
@@ -9,3 +39,56 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+
+
+// Logical operators examples
+
+// Example 5
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+// Example 6
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+// Example 7
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
