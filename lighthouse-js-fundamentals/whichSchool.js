ee99e47d4487bd2867174024fe181ca88b7f4151
Jeandre Visser <jeandrev1414@gmail.com>
2022-02-08 23:01:26 +0000

    Added whichSchool assignment

---
+++ lighthouse-js-fundamentals/whichSchool.js
@@ -0,0 +1,19 @@
+// Function of which school to go to based on age
+
+const whichSchool = function (age) {
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  }
+}
+
+
+console.log("I am 35. Which school should I go to?");
+console.log(whichSchool(35));
+console.log("I am 8. Which school should I go to?");
+console.log(whichSchool(8));
+console.log("I am 14. Which school should I go to?");
+console.log(whichSchool(14));
