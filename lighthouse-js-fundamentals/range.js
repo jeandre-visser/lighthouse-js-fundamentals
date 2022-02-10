22b4edb8c2c3930367fe26f3c5b7a9982cc2e631
Jeandre Visser <jeandrev1414@gmail.com>
2022-02-10 23:49:18 +0000

    Updated

--- lighthouse-js-fundamentals/range.js
+++ lighthouse-js-fundamentals/range.js
@@ -4,8 +4,9 @@ const range = (start, end, step) => {
     rangeArray.push(i)
   }
   return rangeArray;
-}
+  
+} 
 
-console.log(range(0, 10, 2));
+console.log(range(0, 10, -1));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
