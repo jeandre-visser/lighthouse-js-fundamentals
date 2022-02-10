30a84ff11ea066e63b809bb29b60a0c713f4d787
Jeandre Visser <jeandrev1414@gmail.com>
2022-02-10 23:11:03 +0000

    Added banana bread ingredients array assignment containing a for loop, while loop, and backwards for loop

---
+++ lighthouse-js-fundamentals/ingredients.js
@@ -0,0 +1,34 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// while loop
+console.log("Ingredients needed for banana bread:");
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// for loop
+console.log("Ingredients needed for banana bread:");
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+// for loop backwards
+console.log("Ingredients needed for banana bread:");
+for (let i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
