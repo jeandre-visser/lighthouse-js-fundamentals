c2fda43b2f05e1d625da636f3c870e31c3ac246c
Jeandre Visser <jeandrev1414@gmail.com>
2022-02-19 22:47:26 +0000

    Corrected looping range assignment

--- lighthouse-js-fundamentals/range.js
+++ lighthouse-js-fundamentals/range.js
@@ -1,12 +1,15 @@
 const range = (start, end, step) => {
   let rangeArray = [];
-  for (let i = start; i <= end; i = i + step) {
-    rangeArray.push(i)
-  }
+  if (start === null || end === null || step === null || start > end || step < 0) {
+    rangeArray = [];
+  } else {
+    for (let i = start; i <= end; i = i + step) {
+      rangeArray.push(i);
+    };
+  };
   return rangeArray;
-  
-} 
+};
 
-console.log(range(0, 10, -1));
+console.log(range(0, 10, 2));
 console.log(range(10, 30, 5));
 console.log(range(-5, 2, 3));
